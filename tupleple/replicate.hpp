dc12e4476ef05a419060bb61fd54d3335c37ebd2
Fuyutsubaki <pushrunkey@gmail.com>
2014-03-08 02:15:51 +0900

    replicate追加

---
+++ tupleple/replicate.hpp
@@ -0,0 +1,43 @@
+#pragma once
+#include"Index.hpp"
+#include"map.hpp"
+#include"tuple.hpp"
+namespace tupleple
+{
+	namespace type_list
+	{
+		namespace impl{
+			template<size_t N, class T>
+			class repricate_impl
+			{
+				template<class>
+				struct trans{ using type = T; };
+			public:
+				using index_type = index::make_N_index<N>;
+				using type = map<trans, index_type>;
+			};
+		}
+		template<size_t N, class T>
+		using repricate = typename impl::repricate_impl<N, T>::type;
+	}
+
+	namespace deteil
+	{
+		template<class,class T>
+		inline const T& do_nothing(const T&x)
+		{
+			return x;
+		}
+		template<size_t N, class T,class ...Idx>
+		inline type_list::repricate<N, T> repricate_impl(const T&x,std::tuple<Idx...>)
+		{
+			return type_list::repricate<N, T>(do_nothing<Idx>(x)...);
+		}
+	}
+	template<size_t N, class T>
+	type_list::repricate<N, T> repricate(const T&x)
+	{
+		return deteil::repricate_impl<N>(x, index::make_N_index<N>());
+	}
+
+}
